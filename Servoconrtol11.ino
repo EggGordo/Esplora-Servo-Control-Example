eb3704ec8b0eb6dad41573d0707aa3570b1f8a18
gogosrcool <jacksonspaugh13@gmail.com>
2017-10-01 17:36:09 -0400

    Added Descriptions for most lines

--- Servoconrtol11.ino
+++ Servoconrtol11.ino
@@ -1,21 +1,18 @@
-#include <Servo.h>
+#include <Servo.h> //Libraries
 #include <Esplora.h>
 
-Servo myservo;
+Servo myservo; //Name of servo
 
-int val; 
+int val; //Something important
 void setup() {
-  myservo.attach(11);
-  Serial.begin(9600);
-  // put your setup code here, to run once:
+  myservo.attach(11); //change this number to which ever pin your servo will be connected to. Pin readout's on README.md
+  Serial.begin(9600); //Start reading serial output
 }
 
 void loop() {
-  int value = Esplora.readSlider();
-  value = map(value, 0, 1023, 0, 180);
-  Serial.println(value);
-  myservo.write(value);
-  delay(1000);
-  // put your main code here, to run repeatedly:
-
+  int value = Esplora.readSlider(); //Begin reading the Esplora's slider
+  value = map(value, 0, 1023, 0, 180); //Translate slider output to servo compatible range
+  Serial.println(value); //Print new number in serial monitor
+  myservo.write(value); //Tell servo new value and order position change
+  delay(1000); //Delay. prevents overloading the Esplora
 }
